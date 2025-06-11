c3ebe46ef077c5a2017acbc1079dbee28a2c08b6
Manuel Garcia <31109774+Mandroide@users.noreply.github.com>
2025-06-11 18:03:30 -0400

    Corrige pipeline

--- src/oulad_etl/db/session.py
+++ src/oulad_etl/db/session.py
@@ -5,5 +5,5 @@ from ..settings import Settings
 
 settings = Settings()
 db_url = f"mysql+pymysql://{settings.db_user}:{settings.db_password}@{settings.db_host}:{settings.db_port}/{settings.db_name}"
-engine = create_engine(settings.db_url, echo=False)
+engine = create_engine(db_url, echo=False)
 SessionLocal = sessionmaker(bind=engine)
--- src/oulad_etl/etl/load.py
+++ src/oulad_etl/etl/load.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
-from oulad_etl.settings import settings
 import pandas as pd
+
+from ..settings import Settings
 
 
 def load(df: pd.DataFrame, table: str = "oulad_raw") -> None:
-    engine = create_engine(settings.db_url, future=True)
+    settings = Settings()
+    db_url = f"mysql+pymysql://{settings.db_user}:{settings.db_password}@{settings.db_host}:{settings.db_port}/{settings.db_name}"
+    engine = create_engine(db_url, future=True)
     with engine.begin() as conn:
         df.to_sql(table, conn, if_exists="replace", index=False)
